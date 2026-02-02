d812432bd56d13fcdbb51c90c32bc10feb48469f
santiago leonardo vera <112565456+chuflo14@users.noreply.github.com>
2026-02-02 18:47:21 -0300

    Fix PWA config type error

--- triviadmin/next.config.ts
+++ triviadmin/next.config.ts
@@ -4,10 +4,6 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
   reloadOnOnline: false,
   swcMinify: true,
   workboxOptions: {
